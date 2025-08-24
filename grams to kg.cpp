7f2fe07dbab7f1e174e8c24337b4a0692e0e827a
sujalgowda0204 <sujalgowda22@gmail.com>
2025-08-24 10:31:35 +0530

    Add files via upload

---
+++ grams to kg.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("convert grams to kg \n");
+    printf("Enter grams u want to convert ");
+    float h ;
+    scanf("%f",&h);
+    h=h /1000;
+    printf("total kg is : %f",h);
+
+     return 0;
+}
