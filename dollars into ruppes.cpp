3f1d5054bf7326a4716edf8e87cd1514c97211d6
sujalgowda0204 <sujalgowda22@gmail.com>
2025-08-24 09:49:47 +0530

    Add files via upload

---
+++ dollars into ruppes.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Changing dollars into rupees \n");
+    printf("Enter the dollers u want to convert ");
+    float h ;
+    scanf("%f",&h);
+    h=h*48;
+    printf("total rupees is : %f",h);
+
+}
