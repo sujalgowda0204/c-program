2ddc827c5a5d2f4e27debf11dc297a3833f0ebdb
sujalgowda0204 <sujalgowda22@gmail.com>
2025-08-24 09:39:58 +0530

    Add files via upload

---
+++ hours to minute.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Changing hours to minutes \n");
+    printf("Enter the hours u want to convert ");
+    float h ;
+    scanf("%f",&h);
+    h=h*60;
+    printf("The time in minutes is : %f",h);
+
+}
